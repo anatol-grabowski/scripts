e1e7815a102f1c3fdc3727423c4e0ea713d64984
tota <grabantot@gmail.com>
2018-08-11 16:22:08 +0300

    massive refactoring of install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,40 +1,84 @@
+#!/bin/bash
 
 downloads_path="$HOME""/Downloads"
 
-sudo apt-get update
+apps_to_apt_install=(
+  git curl jq ffmpeg gitk
+  doublecmd-gtk gparted vlc vim deluge
+  xclip xdotool xbindkeys
+  build-essential cmake pkg-config
+  libx11-dev libatlas-base-dev libgtk-3-dev
+  python libboost-python-dev python-dev python-pip python3-dev python3-pip
+)
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt install google-chrome-stable
+additional_apps_to_apt_install=(
+  wine-stable
+  libgstreamer1.0-0 gstreamer1.0-plugins-base
+  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
+  gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
+)
 
-sudo apt install git curl jq xclip ffmpeg gitk
-sudo apt install doublecmd-gtk gparted vlc vim deluge wine
-sudo apt install build-essential cmake pkg-config
-sudo apt install libx11-dev libatlas-base-dev libgtk-3-dev
-sudo apt install python libboost-python-dev python-dev python-pip python3-dev python3-pip
-sudo apt install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools # not sure why I need this
+function apt_install_not_installed() {
+  apps=("$@")
+  not_installed_apps=()
+  for app in "${apps[@]}" ; do
+    dpkg -l "$app" > /dev/null 2>&1 \
+    && echo "package already installed: ""$app" \
+    || not_installed_apps+=("$app")
+  done
+
+  if [ "${#not_installed_apps[@]}" -ne 0 ] ; then
+    echo "going to install: ""${not_installed_apps[@]}"
+    sudo apt-get update
+    sudo apt install "${not_installed_apps[@]}"
+  fi
+}
+
+function install_vscode_ext_from_github() {
+  # repo name: "user/repo"
+  # downlad path: "/home/user/Downloads"
+  url="https://api.github.com/repos/""$1""/releases/latest"
+  dowurl=`curl --silent "$url" | jq -r .assets[0].browser_download_url`
+  filename=`curl --silent "$url" | jq -r .assets[0].name`
+  dowpath="$2"/"$filename"
+  wget -O "$dowpath" "$dowurl"
+  code --install-extension "$dowpath"
+}
 
 
+apt_install_not_installed ${apps_to_apt_install[@]}
+apt_install_not_installed ${additional_apps_to_apt_install[@]}
 
-wget -O- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-# . ~/.bashrc
-# nvm install 10
+if ! which "google-chrome" ; then
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub \
+  | sudo apt-key add -
+  echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' \
+  | sudo tee /etc/apt/sources.list.d/google-chrome.list
+  sudo apt-get update
+  sudo apt install google-chrome-stable
+fi
 
-# sudo dpkg -i $HOME/Downloads/code_*.deb
-# sudo apt -f install
-# sudo dpkg -i $HOME/Downloads/code_*.deb
+if ! which "code" ; then
+  microsoft_gpg_download_path="$downloads_path"/microsoft.gpg
+  curl https://packages.microsoft.com/keys/microsoft.asc \
+  | gpg --dearmor > "$microsoft_gpg_download_path"
+  sudo cp "$microsoft_gpg_download_path" "/etc/apt/trusted.gpg.d/microsoft.gpg"
+  sudo sh -c 'echo "deb [arch=amd64] \
+  https://packages.microsoft.com/repos/vscode stable main" \
+  > /etc/apt/sources.list.d/vscode.list'
+  sudo apt-get update
+  sudo apt-get install code
+fi
 
-microsoft_gpg_download_path="$downloads_path"/microsoft.gpg
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > "$microsoft_gpg_download_path"
-sudo cp "$microsoft_gpg_download_path" "/etc/apt/trusted.gpg.d/microsoft.gpg"
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
-sudo apt-get install code
+if ! code --list-extensions | grep "gitlens" ; then
+  install_vscode_ext_from_github "eamodio/vscode-gitlens" "$downloads_path"
+fi
+if ! code --list-extensions | grep "bracket-pair-colorizer" ; then
+  install_vscode_ext_from_github "CoenraadS/BracketPair" "$downloads_path"
+fi
 
-gitlens_url="https://api.github.com/repos/eamodio/vscode-gitlens/releases/latest"
-gitlens_dowurl=`curl --silent "$gitlens_url" | jq -r .assets[0].browser_download_url`
-gitlens_filename=`curl --silent "$gitlens_url" | jq -r .assets[0].name`
-gitlens_dowpath="$downloads_path"/"$gitlens_filename"
-wget -O "$gitlens_dowpath" "$gitlens_dowurl"
-code --install-extension "$gitlens_dowpath"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+if !  command -v "nvm" ; then
+  nvmurl="https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh"
+  wget -O- "$nvmurl" | bash
+fi
