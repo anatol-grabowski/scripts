7768708e77ff722d3c455e81f40481b0b2e985a0
tot <grabantot@gmail.com>
2019-03-10 22:44:33 +0300

    dconf backup/restore

--- configure/backup-settings.sh
+++ configure/backup-settings.sh
@@ -2,6 +2,8 @@
 
 configs_list_file=$(dirname "$0")"/config-files-list.txt"
 dest_dir_path=$(dirname "$0")"/../settings"
+dconf_pathes_file_path=$(dirname "$0")"/dconf-keys.txt"
+dconf_backup_file_path="$dest_dir_path/dconf-dump-vals.txt"
 
 backup_config () {
   # echo config line "$1"
@@ -14,9 +16,21 @@ backup_config () {
   cp "$src_path" "$dest_full_path"
 }
 
+backup_dconf_key () {
+  dconf_dir=$(dirname "$1")
+  dconf_key=$(basename "$1")
+  key_val="$dconf_dir/"$(dconf dump "$dconf_dir/" | grep "^$dconf_key=")
+  echo Dconf backup: "$key_val"
+  echo "$key_val" >> "$dconf_backup_file_path"
+}
+
 
 echo "$dest_dir_path"
-cat "$configs_list_file" | envsubst | while read config
-do
+cat "$configs_list_file" | envsubst | while read config; do
   backup_config "$config"
+done
+
+echo -n "" > "$dconf_backup_file_path"
+cat "$dconf_pathes_file_path" | while read dconf_path; do
+  backup_dconf_key "$dconf_path"
 done
--- configure/restore-settings.sh
+++ configure/restore-settings.sh
@@ -2,6 +2,8 @@
 
 configs_list_file=$(dirname "$0")"/config-files-list.txt"
 dest_dir_path=$(dirname "$0")"/../settings"
+dconf_pathes_file_path=$(dirname "$0")"/dconf-keys.txt"
+dconf_backup_file_path="$dest_dir_path/dconf-dump-vals.txt"
 
 backup_config () {
   # echo config line "$1"
@@ -13,9 +15,20 @@ backup_config () {
   cp "$dest_full_path" "$src_path"
 }
 
+restore_dconf_key () {
+  dconf_path=$(echo "$1" | awk -F "=" '{print $1}')
+  dconf_val=$(echo "$1" | awk -F "=" '{print $2}')
+  echo Dconf write: "$dconf_path" "$dconf_val"
+  dconf write "$dconf_path" "$dconf_val"
+}
+
 
 echo "$dest_dir_path"
 cat "$configs_list_file" | envsubst | while read config
 do
   backup_config "$config"
+done
+
+cat "$dconf_backup_file_path" | while read path_val; do
+  restore_dconf_key "$path_val"
 done
