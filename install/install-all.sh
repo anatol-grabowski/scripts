fbaa0335ecf860e6dd0f330c667ca0e70f1a4298
tot <grabantot@gmail.com>
2019-03-10 11:54:23 +0300

    hub

--- install/install-all.sh
+++ install/install-all.sh
@@ -14,7 +14,7 @@ apps_to_apt_install=(
 )
 
 additional_apps_to_apt_install=(
-  wine-stable
+  wine-stable openjdk-11-jdk
   libgstreamer1.0-0 gstreamer1.0-plugins-base
   gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
   gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
---
+++ install/install-hub.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:cpick/hub
+sudo apt-get update
+sudo apt-get install hub
